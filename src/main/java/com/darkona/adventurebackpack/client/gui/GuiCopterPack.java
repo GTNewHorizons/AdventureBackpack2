131ffcdbf43e6d4331769ec97f1c99b3cfea0f85
rjnasers <rjnasers@iastate.edu>
2026-08-22 04:53:27 +0200

    Fixing tank rendering

--- src/main/java/com/darkona/adventurebackpack/client/gui/GuiCopterPack.java
+++ src/main/java/com/darkona/adventurebackpack/client/gui/GuiCopterPack.java
@@ -69,12 +69,14 @@ public class GuiCopterPack extends GuiWithTanks {
         GL11.glDisable(GL11.GL_BLEND);
         inventory.openInventory();
         FluidTank tank = inventory.getFuelTank();
+        int displayedAmount = ((ContainerCopter) inventorySlots).getInterpolatedFuelAmount(tank.getCapacity());
+        tank.getFluid().amount = displayedAmount;
         fuelTank.draw(this, tank);
 
         GL11.glPushMatrix();
         boolean isFilled = tank.getFluid() != null;
         String name = isFilled ? WordUtils.capitalize(tank.getFluid().getLocalizedName()) : "None";
-        int displayedAmount = ((ContainerCopter) inventorySlots).getInterpolatedFuelAmount(tank.getCapacity());
+        
         String amount = isFilled ? "" + displayedAmount : "0";
         String capacity = Integer.toString(tank.getCapacity());
         int offsetY = 8;
